58d2d3da9c240c379603dc6350a33f7cc20f8fa4
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2025-06-22 04:17:15 +0530

    fix auth

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-import type { NextConfig } from "next";
 import { createCivicAuthPlugin } from "@civic/auth/nextjs";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,7 +9,7 @@ const withCivicAuth = createCivicAuthPlugin({
   clientId: process.env.CIVIC_AUTH_CLIENT_ID!,
   loginUrl: "/signin",
   loginSuccessUrl: "/callback",
-  logoutUrl: "/",
+  exclude: ["/", "/signup"],
 });
 
 export default withCivicAuth(nextConfig);
